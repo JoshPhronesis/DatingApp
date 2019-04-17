ca3266b00e4f78b9a22b40872db3b9e8ea2bcb3d
Joshua Akosa <Joshua.Akosa@alza.cz>
2019-04-17 13:47:56 +0200

    Add Client side Login adn Register

---
+++ DatingApp-SPA/src/app/home/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(()=>{
+      console.log('reg successful');
+    }, error => {
+      console.log(error);
+    });
+
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('canceled');
+  }
+}
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API.Controllers
 
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> GetValues()
         {
             var values = await _context.Values.ToListAsync();
